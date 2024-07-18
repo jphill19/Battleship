7ad99307242c5cb7e87100b6349740dcc2f71ad4
John Hill <jphill1989@gmail.com>
2024-07-18 16:03:43 -0600

    Add empty? method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,14 @@
 class Cell
 
-  attr_reader :coordinate
+  attr_reader :coordinate, :ship
   
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
+  end
+
+  def empty?
+    @ship != nil
   end
   
 end
